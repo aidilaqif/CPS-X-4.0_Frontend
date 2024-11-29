f0903f718fe53a261694f92dad9e2cf290621131
HARITH DZIKRI BIN HASHIM <harithdzikri12@gmail.com>
2024-11-29 15:08:01 +0800

    added autopilot functions (wrong branch name)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import DroneInterface from "./components/DroneInterface";
+import AutoPilot from "./components/AutoPilot";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <DroneInterface />
-    </div>
+    <Router>
+      <div>
+        <nav className="navigation">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+            end
+          >
+            Manual Control
+          </NavLink>
+          <NavLink
+            to="/autopilot"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            AutoPilot
+          </NavLink>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<DroneInterface />} />
+          <Route path="/autopilot" element={<AutoPilot />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
