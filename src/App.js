c27a0f65fe6002fc1ba15b00bedf97ae26932ac3
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2024-12-23 11:59:13 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 // src/App.js
 import React, { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import DroneInterface from "./components/DroneInterface";
 import AutoPilot from "./components/AutoPilot";
-<<<<<<< Updated upstream
 import NavigationTab from "./components/navigation/NavigationTab";
 import ItemManagement from "./components/items/ItemManagement";
 import LocationManagement from "./components/locations/LocationManagement";
 import Export from "./components/exports/Export";
 import "./assets/styles/index.css";
-=======
-import NavigationTab from './components/navigation/NavigationTab';
-import ItemManagement from './components/items/ItemManagement';
-import LocationManagement from './components/locations/LocationManagement';
-import Export from './components/exports/Export';
 import './assets/styles/index.css';
-// import "./App.css";
-
->>>>>>> Stashed changes
 
 function App() {
   const [currentPage, setCurrentPage] = useState("drone");
